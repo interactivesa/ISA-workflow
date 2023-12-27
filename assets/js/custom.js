d47db913e621cd632ada2c2691a2cfda29e50802
Mahmoud <oudy_mosa@hotmail.com>
2023-12-27 12:39:38 +0200

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,84 @@
+"use strict";
+
+var scrollTo = function (sectionEl = "", callback = function () {}) {
+  $("html, body").animate(
+    {
+      scrollTop: sectionEl ? calculateOffset(sectionEl) : 0,
+    },
+    800,
+    "swing",
+    callback()
+  );
+};
+var calculateOffset = function (el) {
+  if ($(el).length == 0) return;
+  if ($(window).width() >= 980) return Math.floor($(el).offset().top - 69);
+  return Math.floor($(el).offset().top);
+};
+// on ready state
+$(function () {
+  // breakpoint and up
+  // $(window).resize(function () {
+  //   if ($(window).width() >= 980) {
+  //     // when you hover a toggle show its dropdown menu
+  //     $(".navbar .dropdown-toggle").hover(function () {
+  //       $(this).parent().toggleClass("show");
+  //       $(this).parent().find(".dropdown-menu").toggleClass("show");
+  //     });
+
+  //     // hide the menu when the mouse leaves the dropdown
+  //     $(".navbar .dropdown-menu").mouseleave(function () {
+  //       $(this).removeClass("show");
+  //     });
+
+  //     // do something here
+  //   }
+  // });
+  $(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    var navbar = $(".main-navbar");
+    var scrollUpBtn = $(".scroll-up");
+    // if (scroll > 0) {
+    //   scrollUpBtn.fadeIn();
+    // } else {
+    //   scrollUpBtn.fadeOut();
+    // }
+    // if ($(window).width() >= 992) {
+    //   if (scroll > 140) {
+    //     if (!navbar.hasClass("fixed")) {
+    //       $("body").css({ "padding-top": navbar.height() + "px" });
+    //       navbar.addClass("fixed");
+    //       // $(".search-toggle").addClass("d-none");
+    //     }
+    //   } else {
+    //     navbar.removeClass("fixed");
+    //     $("body").css({ "padding-top": "0px" });
+    //     $(".search-toggle").removeClass("d-none");
+    //   }
+    // }
+ 
+   
+  });
+
+
+
+
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+// bootstrap tooltip
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
